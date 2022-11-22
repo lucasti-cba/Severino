559d43e9bb6284116e6f4ae767b64bb9aae76be1
Lucas A. Batista <lucasti-cba@outlook.com>
2022-11-22 01:32:30 -0400

    Adicionar arquivos de projeto.

---
+++ Severino/Severino/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Severino.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Severino.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
