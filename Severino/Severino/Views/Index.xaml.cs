f1b6f85af0e0d9732eeb7976b0a9edeaea0286d9
Lucas A. Batista <lucasti-cba@outlook.com>
2022-11-28 20:22:21 -0400

    Layouts

--- Severino/Severino/Views/Index.xaml.cs
+++ Severino/Severino/Views/Index.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Severino.ContentViews;
+using Severino.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -9,7 +10,7 @@ namespace Severino.Views
 {
     public partial class IndexPage : ContentPage
     {
-      
+        ItemsViewModel _viewModel;
         public IndexPage()
         {
 
@@ -24,6 +25,17 @@ namespace Severino.Views
                 new Models.Servicos(){  Name = "Jardinagem", ImageSource =  ImageSource.FromUri(new Uri("https://img.icons8.com/external-vitaliy-gorbachev-lineal-color-vitaly-gorbachev/512/external-plumber-labour-day-vitaliy-gorbachev-lineal-color-vitaly-gorbachev.png"))  },
                 new Models.Servicos(){  Name = "Limpeza" , ImageSource =  ImageSource.FromUri(new Uri("https://img.icons8.com/external-vitaliy-gorbachev-lineal-color-vitaly-gorbachev/512/external-plumber-labour-day-vitaliy-gorbachev-lineal-color-vitaly-gorbachev.png")) },
             };
+
+            ListLiked.ItemsSource = new Models.Servicos[]
+            {
+                new Models.Servicos(){ Name = "Hidraulica" , ImageSource =  ImageSource.FromUri(new Uri("https://img.icons8.com/external-vitaliy-gorbachev-lineal-color-vitaly-gorbachev/512/external-plumber-labour-day-vitaliy-gorbachev-lineal-color-vitaly-gorbachev.png"))},
+                new Models.Servicos(){  Name = "Eletrica" , ImageSource =  ImageSource.FromUri(new Uri("https://img.icons8.com/external-vitaliy-gorbachev-lineal-color-vitaly-gorbachev/512/external-plumber-labour-day-vitaliy-gorbachev-lineal-color-vitaly-gorbachev.png"))},
+                new Models.Servicos(){  Name = "Climatizacao"  , ImageSource =  ImageSource.FromUri(new Uri("https://img.icons8.com/external-vitaliy-gorbachev-lineal-color-vitaly-gorbachev/512/external-plumber-labour-day-vitaliy-gorbachev-lineal-color-vitaly-gorbachev.png"))},
+                new Models.Servicos(){  Name = "Jardinagem", ImageSource =  ImageSource.FromUri(new Uri("https://img.icons8.com/external-vitaliy-gorbachev-lineal-color-vitaly-gorbachev/512/external-plumber-labour-day-vitaliy-gorbachev-lineal-color-vitaly-gorbachev.png"))  },
+                new Models.Servicos(){  Name = "Limpeza" , ImageSource =  ImageSource.FromUri(new Uri("https://img.icons8.com/external-vitaliy-gorbachev-lineal-color-vitaly-gorbachev/512/external-plumber-labour-day-vitaliy-gorbachev-lineal-color-vitaly-gorbachev.png")) },
+            };
+
+            BindingContext = _viewModel = new ItemsViewModel();
 
         }
     }
